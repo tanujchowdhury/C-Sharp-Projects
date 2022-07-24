31a2dc35316024e441e78c2034045434e297cb48
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-23 18:37:46 -0700

    Added while loops assignment

---
+++ WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Create a counter
+        int i = 1;
+
+        //While loop to count 1, 2, 3
+        while (i < 4)
+        {
+            Console.WriteLine(i + ",");
+            i++;
+        }
+
+        //Says "Go" once because condition is already met
+        do
+        {
+            Console.WriteLine("Go!");
+        }
+        while (i < 3);
+    }
+}
+
