cf6e4a20b75c5b6bc7a493b0568ea54bd5bea7d8
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-25 16:58:09 -0700

    Added optional param assignment

---
+++ OptionalParam/OptionalParam/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionalParam
+{
+    class Methods
+    {
+        //Method with an optional second param. Takes int as params, returns int
+        public static int Multiply(int num1, int num2 = 10)
+        {
+            int res = num1 * num2;
+            return res;
+        }
+    }
+}
---
+++ OptionalParam/OptionalParam/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static OptionalParam.Methods;
+
+namespace OptionalParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get an input from the user, user must give input here
+            Console.WriteLine("Enter a number (1/2):");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Ask user for another input, this is optional
+            Console.WriteLine("Enter another number *optional* (2/2):");
+
+            //If user does not give 2nd input, a FormatException will occuer
+            try
+            {
+                //If both inputs are given, use both inputs as params
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Result: " + Multiply(num1, num2));
+            }
+            catch
+            {
+                //If only 1st input is given, use that input as the only param
+                Console.WriteLine("Result:" + Multiply(num1));
+            }
+            Console.ReadLine();
+        }
+    }
+}
