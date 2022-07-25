973a84179629fdeb3e51416ef183310e7253848a
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-25 16:25:56 -0700

    Added method overloading assignment

---
+++ MethodOverloading/MethodOverloading/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverloading
+{
+    class Methods
+    {
+        //Passes in int, does addition, returns int
+        public static int Math10(int num)
+        {
+            int res = num + 10;
+            return res;
+        }
+
+        //Passes in decimal, does multiplication, converts decimal to int, returns int
+        public static int Math10(decimal num)
+        { 
+            decimal res = num * 10;
+            int resInt = Convert.ToInt32(res);
+            return resInt;
+        }
+
+        //Passes in string, converts string to int, does subtraction, returns int
+        public static int Math10(string num)
+        {
+            int numInt = Convert.ToInt32(num);
+            int res = numInt - 10;
+            return res;
+        }
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static MethodOverloading.Methods;
+
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demonstrates method overloading.
+            //Each uses same method name, but a different data type is passed,
+            //resulting in a different method being called
+           
+            //returns 20: 10 + 10
+            Console.WriteLine(Math10(10));
+
+            //returns 424: 42.42 x 10, truncated
+            Console.WriteLine(Math10(42.42m));
+
+            //returns 351: 361 - 10
+            Console.WriteLine(Math10("361"));
+        }
+    }
+}
