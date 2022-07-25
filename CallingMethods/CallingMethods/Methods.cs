1b9b44a3bcd95b942b56daaab7a5e82b4621f5aa
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-25 15:47:10 -0700

    Added calling methods assignment

---
+++ CallingMethods/CallingMethods/Methods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethods
+{
+    public class Methods
+    {
+        public static int Add42(int num)
+        {
+            int res = num + 42;
+            return res;
+        }
+        public static int Mult100(int num)
+        {
+            int res = num * 100;
+            return res;
+        }
+        public static int Sub27(int num)
+        {
+            int res = num - 27;
+            return res;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//Let this .cs file see the methods in the other .cs file
+using static CallingMethods.Methods;
+
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask user for input
+            Console.WriteLine("Enter a whole number to do math operations on:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //Call each method, pass in the user input, and display the result
+            Console.WriteLine(input + " plus 42 is " + Add42(input));
+            Console.WriteLine(input + " times 100 is " + Mult100(input));
+            Console.WriteLine(input + " minus 27 is " + Sub27(input));
+            Console.ReadLine();
+        }
+    }
+}
