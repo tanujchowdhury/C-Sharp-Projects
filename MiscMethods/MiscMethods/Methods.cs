c77af9657ddb4893b1356bad4ac9c7475bd20b54
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-26 13:36:58 -0700

    Added misc methods assignment

---
+++ MiscMethods/MiscMethods/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiscMethods
+{
+    class Methods
+    {
+        //Void method that outputs an integer
+        //Method has output param
+        //Method is overloaded
+        public static void Div2(int num, out string Message)
+        {
+            int res = num / 2;
+            Message = "Your number was divided by 2";
+            Console.WriteLine(res);
+        }
+        public static decimal Div2(decimal num)
+        {
+            decimal res = num / 2;
+            return res;
+        }
+
+        //Class marked static
+        public static class Foo {
+            //code
+        }
+        
+    }
+}
---
+++ MiscMethods/MiscMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static MiscMethods.Methods;
+
+namespace MiscMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The method is called on the user's input
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string Message;
+            Div2(num, out Message);
+
+        }
+    }
+}
