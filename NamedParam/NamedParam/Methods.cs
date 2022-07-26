1aa4870462f566cd9cf694f21b7bf83856152bd7
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-25 17:14:45 -0700

    Add named params assginment

---
+++ NamedParam/NamedParam/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamedParam
+{
+    public class Methods
+    {
+        //A method that takes two int params and returns nothing
+        public static void TwoNums(int num1, int num2)
+        {
+            int num3 = num1 * 1024;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ NamedParam/NamedParam/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static NamedParam.Methods;
+
+namespace NamedParam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calls the method
+            TwoNums(10, 15);
+            //Call the method, with param names specified
+            TwoNums(num1: 10, num2: 15);
+        }
+    }
+}
