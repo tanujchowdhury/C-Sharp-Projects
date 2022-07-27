9377a1d82c633f6ddfff7428a42e56c1d60047ee
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-27 13:20:45 -0700

    Added operator overloading assignment

---
+++ OperatorOverloading/OperatorOverloading/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorOverloading
+{
+    class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        //Operator overloading, == checks for 2 employee ids being equal
+        public static bool operator== (Employee employee1, Employee employee2)
+        {
+            if (employee1.ID == employee2.ID)
+            {
+                return true;
+
+            //Else statement is required or we get "not all code paths return a value"
+            } else
+            {
+                return false;
+            }
+        }
+
+        //Operator overloading, != checks for 2 employee ids being different
+        public static bool operator!= (Employee employee1, Employee employee2)
+        {
+            if (employee1.ID != employee2.ID)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Test to see if new operator works
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+            Employee e3 = new Employee();
+            e1.ID = 1;
+            e2.ID = 2;
+            e3.ID = 2;
+            
+
+            Console.WriteLine(e1 == e2); //False
+            Console.WriteLine(e1 != e2); //True
+            Console.WriteLine(e2 == e3); //True
+
+        }
+    }
+}
