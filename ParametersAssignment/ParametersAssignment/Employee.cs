e252ce010f2c56bda1cd41040e34a94093b64b5e
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-27 13:53:59 -0700

    Added parameters assignment

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParametersAssignment
+{
+    public class Employee<T>
+    {
+        public List<T> Things { get; set; }
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>();
+            employee1.Things = new List<string> { "test1", "test2", "test3" };
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int> { 1, 2, 3 };
+
+            foreach (string thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach (int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
