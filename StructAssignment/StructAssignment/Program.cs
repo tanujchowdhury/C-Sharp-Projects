dbe45971225d1da259c58ef6defb18726c2293e1
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-31 16:22:35 -0700

    Added struct assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        struct Number
+        {
+            public decimal Amount;
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Number myNum;
+            myNum.Amount = 10.5m;
+            Console.WriteLine(myNum.Amount);
+
+        }
+    }
+}
