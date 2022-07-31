f7c785e284fd60e0e4fa73b614e1f25a6d85b031
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-31 16:02:18 -0700

    Added parsing enums assignment

---
+++ ParsingEnums/ParsingEnums/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParsingEnums
+{
+    //Enum for days of the week
+    public enum DaysEnum
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+}
---
+++ ParsingEnums/ParsingEnums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ParsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Store user input as a string for now
+            Console.WriteLine("Enter the current day of the week:");
+            string input = Console.ReadLine();
+
+            try
+            {
+                //myDay has type DaysEnum; using Parse method to store user input in this var
+                DaysEnum myDay = (DaysEnum)Enum.Parse(typeof(DaysEnum), input);
+                Console.WriteLine(myDay);
+            }
+            catch
+            {
+                //If any error occurs, display this message
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            
+
+
+        }
+    }
+}
