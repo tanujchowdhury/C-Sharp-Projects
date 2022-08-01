8d8f9c336faccb3cd373bb4a5c74e6cbe33be2d6
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-31 23:15:41 -0700

    Added constructors assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare a constant
+            const string fullName = "Tanuj Chowdhury";
+
+            //Declare a variable using "var"
+            var yearsOld = 27;
+
+            //Instantiate 3 users, each with different parameters so different constructors will be used
+            User user1 = new User();
+            User user2 = new User(fullName);
+            User user3 = new User(fullName, yearsOld);
+
+            //Test to see if users were instantiated properly
+            Console.WriteLine("{0}, {1}", user1.Name, user1.Age);
+            Console.WriteLine("{0}, {1}", user2.Name, user2.Age);
+            Console.WriteLine("{0}, {1}", user3.Name, user3.Age);
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructorAssignment
+{
+    public class User
+    {
+        public string Name;
+        public int Age;
+
+        //Chaining constructors
+        public User() : this("", 0)
+        {
+        }
+        public User(string Name) : this(Name, 0)
+        {
+        }
+        public User(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
