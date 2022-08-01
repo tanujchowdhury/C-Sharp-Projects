a2477a9fca155553b95ff1e786f90a6c9ce0448a
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-07-31 22:10:50 -0700

    Added date/time assignment

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints current date and time to console
+            Console.WriteLine(DateTime.Now);
+
+            //Asks user for a number
+            Console.WriteLine("Enter a number:");
+
+            //Converts user input to double, then adds that many hours to current date and time
+            Console.WriteLine(DateTime.Now.AddHours(Convert.ToDouble(Console.ReadLine())));
+        }
+    }
+}
