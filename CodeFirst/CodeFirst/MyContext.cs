46d6e84a681010fb8e6051fba24291b17b0ab8ac
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-08-03 23:10:52 -0700

    Added code-first challenge assignment

---
+++ CodeFirst/CodeFirst/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class MyContext : DbContext
+    {
+        public virtual DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Data.Entity;
+
+namespace EFCodeFirstDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyContext())
+            {
+                // Create and save a new Students
+                Console.WriteLine("Adding new students");
+
+                var student = new Student
+                {
+                    FirstMidName = "Bruce",
+                    LastName = "Wayne",
+                };
+
+                context.Students.Add(student);
+
+                var student1 = new Student
+                {
+                    FirstMidName = "Peter",
+                    LastName = "Parker",
+                };
+
+                context.Students.Add(student1);
+                context.SaveChanges();
+
+                // Display all Students from the database
+                var students = (from s in context.Students
+                                orderby s.FirstMidName
+                                select s).ToList<Student>();
+
+                Console.WriteLine("Retrieve all Students from the database:");
+
+                foreach (var stdnt in students)
+                {
+                    string name = stdnt.FirstMidName + " " + stdnt.LastName;
+                    Console.WriteLine("ID: {0}, Name: {1}", stdnt.ID, name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+    }
+
+    public class MyContext : DbContext
+    {
+        public virtual DbSet<Student> Students { get; set; }
+    }
+
+}
