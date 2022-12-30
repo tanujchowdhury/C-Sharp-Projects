f7d3fa77123f96419845afca8c20bb11dd33ba66
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2022-12-30 10:38:42 -0800

    add arightco website test

---
+++ Arightco/Arightco/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Arightco.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
